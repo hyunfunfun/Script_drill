2c76bc2844021777aa474917229057892a4aa91a
hyunfunfun(gram) <hyunfunfun@naver.com>
2024-04-03 15:57:24 +0900

    lab 8-1

---
+++ lab/ch8-2.py
@@ -0,0 +1,14 @@
+import pickle
+
+colors = ['red','green']
+f=open('ch8_2.txt','wb')
+pickle.dump(colors,f)
+f.close()
+
+del colors
+
+f=open('ch8_2.txt','rb')
+colors=pickle.load(f)
+f.close()
+
+print(colors)
